9d19899dc4e5df2f15660d4a3103178c651ce602
adm41597 <admichel415@yahoo.com>
2018-09-13 13:45:13 -0500

    Trying to figure dynamic graphing

--- tryOne.py
+++ tryOne.py
@@ -1,6 +1,7 @@
 from bokeh.layouts import layout
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import Button, Slider, TextInput, WidgetBox, AjaxDataSource
+from bokeh.document.events import ColumnsStreamedEvent
 import random
 import smtplib
 
@@ -19,39 +20,42 @@ server.starttls()
 server.quit()
 
 
-def plot():
-    p = figure(plot_width=1200, plot_height=800, x_range=(0,300), y_range=(30,100))
+#def plot():
+p = figure(plot_width=1200, plot_height=800, x_range=(0,300), y_range=(30,100))
 
-    x_axis = list(range(0,301))
+x_axis = list(range(0,300))
 
-    y_axis=[]
-    nan=float('nan')
+y_axis=[]
+nan=float('nan')
 
-    i=0
-    while i<301:
-        y_axis.append(nan)
-        i+=1
+i=0
+while i<300:
+    y_axis.append(nan)
+    i+=1
 
 #print("got to here")
 #p.line(x_axis, y_axis, line_width=2)
 #p.circle(x_axis, y_axis, fill_color="white", size=8)
 #show(p)
 
-    i=0
-    while i<301:
-        n=random.randint(30,100)
-        y_axis.append(n)
-        y_axis.pop(0)
-        i+=1
+#i=0
+#while i<301:
+#    n=random.randint(30,100)
+#    y_axis.append(n)
+#    y_axis.pop(0)
+#    i+=1
 #    p.line(x_axis, y_axis, line_width=2)
 #    p.circle(x_axis, y_axis, fill_color="white", size=8)
 #    p.update()
 #print("got to here")
 
-    p.line(x_axis, y_axis, line_width=2)
-    p.circle(x_axis, y_axis, fill_color="white", size=8)
+p.line(x_axis, y_axis, line_width=2)
+p.circle(x_axis, y_axis, fill_color="white", size=8)
     #show(p)
-    return p
+    #return p
+
+def updatePlot():
+
 
 
 def slider():
